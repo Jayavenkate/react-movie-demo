a5486246a8db81bf5bf4b699687cb1a1e7008da7
Jayavenkate <jayajaikithuja@gmail.com>
2023-05-29 12:53:13 +0300

    edit

--- src/global.js
+++ src/global.js
@@ -1,6 +1,2 @@
-// export const API="https://63dfc41859bccf35dab93fd4.mockapi.io";
-
-export const API="http://localhost:4000";
-// export const API="https://mongodb-node-movies-app.vercel.app";
-
-
+// export const API="http://localhost:4000";
+export const API = "https://mongodb-node-movies-app.vercel.app";
